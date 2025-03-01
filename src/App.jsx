ed1bb9088dd34eda42409fe114afafe9858c6e8b
aayushsoodhp@gmail.com <aayushsoodhp@gmail.com>
2025-03-01 12:09:42 +0530

    Home page routed created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Routes , Route } from 'react-router-dom'
 import './App.css'
 import NewsList from './pages/news-list'
 import NotFound from './pages/NotFound'
+import HomePage from './pages/home'
 
 function App() {
   return (
     <Routes>
       <Route path="/news" element={<NewsList />}></Route>
+      <Route path="/home" element={<HomePage />}></Route>
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
   )
